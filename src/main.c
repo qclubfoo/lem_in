b91758c372c220592db39816965741bdcdd67947
sbrella <tsupickov.ilya@gmail.com>
2019-07-10 19:41:50 +0300

    MAIN

--- src/main.c
+++ src/main.c
@@ -6,13 +6,18 @@
 /*   By: sbrella <sbrella@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/07/10 19:10:34 by sbrella           #+#    #+#             */
-/*   Updated: 2019/07/10 19:19:25 by sbrella          ###   ########.fr       */
+/*   Updated: 2019/07/10 19:41:38 by sbrella          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../inc/lem_in.h"
 
-int main(void)
+t_map	*connected(t_map *map)
+{
+	
+}
+
+int		 main(void)
 {
 	t_map   *map;
 	t_map	*way;
@@ -22,7 +27,7 @@ int main(void)
 		ft_printf("%s\n", map->file);
 	else
 	{
-		/* code */
+		/* error */
 		exit (0);
 	}
 	free(map->file);
